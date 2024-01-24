7ebb99fe4691602baed1b4712e269595782fcc12
max <max.yuri.13@hotmail.com>
2024-01-24 10:22:24 -0300

    Creation of the user table and migrations, stabilizing a connection with the database.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-import express from 'express'
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-export const app = express()
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Max',
+    surname: 'Sousa',
+    email: 'max@email.com'
+  }
+})
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,5 @@
 import { app } from './app'
 import { env } from './env'
-import { sequelize } from './lib/sequelize'
-
-app.get('/', async (req, res) => {
-  sequelize
-    .getQueryInterface()
-    .showAllSchemas()
-    .then((tableObj) => {
-      console.log('Tables in database', '==========================')
-      console.log(tableObj)
-    })
-    .catch((err) => {
-      console.log('showAllSchemas ERROR', err)
-    })
-
-  res.send('Lets go Força da Natureza!')
-})
 
 app.listen(env.PORT, () => {
   console.log('🔥🔥🔥 HTTP Server Running 🔥🔥🔥')
