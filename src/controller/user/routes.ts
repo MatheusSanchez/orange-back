700a8e8d79771e797f284167630e169ee1e22e10
Matheus Sanchez <matheus_sanchez@usp.br>
2024-01-24 17:04:49 -0300

    Route Get user by email

--- src/controller/user/routes.ts
+++ src/controller/user/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { getUserById } from './getUserById'
+import { getUserByEmail } from './getUserByEmail'
 
 export async function userRoutes(app: FastifyInstance) {
 
   app.get('/user/:id', getUserById)
+  app.get('/user', getUserByEmail)
 }
