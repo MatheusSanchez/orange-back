74c467df3d0981032ead4713f00e0ea05daec8e6
Matheus Sanchez <matheus_sanchez@usp.br>
2024-01-25 20:51:45 -0300

    fixing lint errors

--- src/controller/user/routes.ts
+++ src/controller/user/routes.ts
@@ -1,10 +1,10 @@
-import { FastifyInstance } from "fastify";
-import { getUserById } from "./getUserById";
-import { getUserByEmail } from "./getUserByEmail";
-import { registerUser } from "./registerUser";
+import { FastifyInstance } from 'fastify'
+import { getUserById } from './getUserById'
+import { getUserByEmail } from './getUserByEmail'
+import { registerUser } from './registerUser'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post('/user', registerUser);
-  app.get("/user/:id", getUserById);
-  app.get("/user", getUserByEmail); 
+  app.post('/user', registerUser)
+  app.get('/user/:id', getUserById)
+  app.get('/user', getUserByEmail)
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,6 +1,6 @@
-import { Prisma, User } from "@prisma/client";
-import { prisma } from "../../lib/prisma";
-import { UserRepository } from "../user-repository";
+import { Prisma, User } from '@prisma/client'
+import { prisma } from '../../lib/prisma'
+import { UserRepository } from '../user-repository'
 
 export class PrismaUsersRepository implements UserRepository {
   async findByEmail(email: string): Promise<User | null> {
@@ -8,19 +8,20 @@ export class PrismaUsersRepository implements UserRepository {
       where: {
         email,
       },
-    });
+    })
 
-    return user;
+    return user
   }
 
   async findById(id: string): Promise<User | null> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = await prisma.user.create({
       data,
-    });
+    })
 
-    return user;
+    return user
   }
 }
