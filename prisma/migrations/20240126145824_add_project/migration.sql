dc71cffce0ce45d1e6fcae3774706b6967c483d8
max <max.yuri.13@hotmail.com>
2024-01-26 12:05:38 -0300

    Correction of reviews, migration files, and project model name.

---
+++ prisma/migrations/20240126145824_add_project/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "tags" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
