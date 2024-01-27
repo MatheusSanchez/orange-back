771b656747e1c4005de36826b8650b6f8848cdff
pedrodecf <pedro012014@gmail.com>
2024-01-27 10:36:21 -0300

    User authentication controller and JWT Token configuration

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,25 @@ import fastify from 'fastify'
 import { userRoutes } from './controller/user/routes'
 import { env } from './env'
 import { ZodError } from 'zod'
+import { authRoutes } from './controller/session/routes'
+import fastifyJwt from '@fastify/jwt'
+import cors from '@fastify/cors'
 
 
 export const app = fastify()
+app.register(cors, {
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+})
 app.register(userRoutes)
+app.register(authRoutes)
 
 
+app.register(fastifyJwt, {
+  secret: 'squad40',
+  // sign: {
+  //   expiresIn: '10s',
+  // },
+})
 
 app.setErrorHandler((error, _, response) => {
   if (env.NODE_ENV !== 'production') {
