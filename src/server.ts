f6fb9da0fb4bfd70e6472f8ad9d76aab768e2519
Matheus Sanchez <matheus_sanchez@usp.br>
2024-01-27 15:30:05 -0300

    Preparing app for deploy :)

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('🔥🔥🔥 HTTP Server Running 🔥🔥🔥')
