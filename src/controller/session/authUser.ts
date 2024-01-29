510c063648e337da0069bac9801ddd67f8cddc10
Matheus Sanchez <matheus_sanchez@usp.br>
2024-01-29 16:19:03 -0300

    Fixing unauthorized response

--- src/controller/session/authUser.ts
+++ src/controller/session/authUser.ts
@@ -2,33 +2,38 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { AuthUserUseCase } from '../../use-cases/authUserUseCase'
 import { z } from 'zod'
 import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { InvalidCredentialsError } from '../../use-cases/errors/InvalidCredentialsError'
 
-export async function authUser( 
+export async function authUser(
   request: FastifyRequest,
   response: FastifyReply,
 ) {
-
   const userRepository = new PrismaUsersRepository()
   const authUserUseCase = new AuthUserUseCase(userRepository)
 
   const AuthUserUseCaseSchema = z.object({
     email: z.string().email(),
-    password: z.string()
+    password: z.string(),
   })
 
-  const { email, password } = AuthUserUseCaseSchema.parse(request.body) 
+  const { email, password } = AuthUserUseCaseSchema.parse(request.body)
 
-  const { user } = await authUserUseCase.execute({ email, password })
+  try {
+    const { user } = await authUserUseCase.execute({ email, password })
 
-  const token = await response.jwtSign(
-    {},
-    {
-      sign: {
-        sub: user.id
+    const token = await response.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
       },
-    },
-  )
+    )
 
-
-  return response.status(200).send({ user, token })    
+    return response.status(200).send({ user, token })
+  } catch (e) {
+    if (e instanceof InvalidCredentialsError) {
+      return response.status(401).send()
+    }
+  }
 }
